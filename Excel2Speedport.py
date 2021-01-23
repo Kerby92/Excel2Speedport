2b8fbc95945ed46022b0ede300c1423bf3461eb0
Kerby92 <70330878+Kerby92@users.noreply.github.com>
2021-01-23 19:58:26 +0100

    Update v1.0.2-alpha
    • Added an exception handler, if excel template was not supplied
    • Added comments for better readability

--- Excel2Speedport.py
+++ Excel2Speedport.py
@@ -1,17 +1,33 @@
+# Import all required functions from the corresponding packes:
+#----------------------------------------------------------------------------------------
 from openpyxl import load_workbook
 from os import system
 
-system("cls")
+system("cls") # Clear the command window, if not already empty
 
-book = load_workbook('Vorlage_Telefonbuch.xlsx')
+
+# Initialize access variables to the *.xlsx file:
+#----------------------------------------------------------------------------------------
+try:
+    book = load_workbook('Vorlage_Telefonbuch.xlsx')
+    
+except Exception as e:
+    print(e); print("")
+    print("Bitte drücken Sie eine beliebige Taste, um die Anwendung zu schließen...")
+    system("pause >nul")
+
+
 sheet = book.active
 
 numContacts = sheet.max_row-1
 
 
+# Check the number of supplied contacts in the *.xlsx and depending on the number,
+# generate a txt or tell the user, that there was no contact supplied.
+#----------------------------------------------------------------------------------------
 if numContacts > 1:
-    print("Es wurden insgesamt "+str(numContacts)+" Kontakte erkannt.\n")
-    f = open("Vorlage_Telefonbuch.txt", mode='w', encoding='utf-8')
+    print("Es wurden insgesamt " + str(numContacts) + " Kontakte erkannt.\n")
+    f = open("Telefonbuch.txt", mode='w', encoding='utf-8')
 
 elif numContacts == 0:
     print("Die Excel-Liste \"Vorlage_Telefonbuch.xlsx\" beinhaltet keine Kontakte.\n")
@@ -21,10 +37,12 @@ elif numContacts == 0:
     exit()
 
 else:
-    print("Es wurde "+str(numContacts)+" Kontakt erkannt.\n")
-    f = open("Vorlage_Telefonbuch.txt", mode='w', encoding='utf-8')
+    print("Es wurde " + str(numContacts) + " Kontakt erkannt.\n")
+    f = open("Telefonbuch.txt", mode='w', encoding='utf-8')
 
 
+# Copy and write the data from the *.xlsx to a *.txt file for any cell of a row:
+#----------------------------------------------------------------------------------------
 counter = 0
 for row in sheet.iter_rows(min_row=1, max_col=10, max_row=sheet.max_row):
 
@@ -44,8 +62,13 @@ for row in sheet.iter_rows(min_row=1, max_col=10, max_row=sheet.max_row):
 
 f.close()
 
+
+# Print out the finish message::
+#----------------------------------------------------------------------------------------
 print("Excel-Liste wurde erfolgreich zu \"Telefonbuch.txt\" im UTF-8 Format konvertiert!")
+
 print("Sie können \"Telefonbuch.txt\" über die Benutzeroberfläche des Speedports \"www.speedport.ip\" importieren. \n")
 
 print("Bitte drücken Sie eine beliebige Taste, um die Anwendung zu schließen...")
-system("pause >nul")
+
+system("pause >nul") # ">nul" suppresses the default message by the system based pause function
